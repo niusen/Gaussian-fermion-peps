50399de862c9e50e4893a8d538385f0f539e9abb
niusen <1501110187@pku.edu.cn>
2022-12-04 21:06:56 +0100

    Update solve_square_dirac.jl

--- solve_square_dirac.jl
+++ solve_square_dirac.jl
@@ -3,7 +3,7 @@ using JSON
 using HDF5, JLD
 using Random
 using Flux
-cd("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\Gaussian_fermion_peps")
+cd("D:\\My Documents\\Code\\Julia_codes\\Tensor network\\Gaussian-fermion-peps")
 
 include("cost_function.jl")
 
@@ -11,9 +11,10 @@ include("cost_function.jl")
 ls_max=20;
 alpha0=2;
 ls_ratio=2/3;
+noise_ite=1;
 
 #Hamiltonian parameters
-Random.seed!(777)
+Random.seed!(666)
 Lx=16;
 Ly=16;
 N=Lx*Ly;
@@ -29,9 +30,12 @@ M=4;#number of virtual modes per bond
 Q=2*M+filling;#total number of physical and virtual fermions on a site; 
 #size of W matrix: (P+4M, Q)
 
-W=rand(P+4*M,P+4*M)+im*rand(P+4*M,P+4*M);
-U,_,_=svd(W);
-W=U[:,1:Q];
+function initial_W(P,M,Q)
+    W=rand(P+4*M,P+4*M)+im*rand(P+4*M,P+4*M);
+    U,_,_=svd(W);
+    W=U[:,1:Q];
+end
+W=initial_W(P,M,Q);
 
 if boundary_x==1
     kxs=Float64.(1:1:Lx/2)*(2*pi)/(Lx/2);
@@ -60,54 +64,42 @@ end
 cost_f(W)=square_dirac(Lx,Ly,P,M,kxs,kys,W);
 
 
+function line_search(W,noise)
 
-Q_G_old=zeros(size(W));
-direction_old=zeros(size(W,1),size(W,1));
-for op_step=1:1000
-    E0=cost_f(W);
-    println("Optimization "*string(op_step)*", E0="*string(E0));flush(stdout);
-    grad = gradient(Flux.params(W)) do
-        cost_f(W)
-    end
-    g=grad[W];
-    Q_G=g*W'-W*g';
-    improved=false;
-    improvement=0;
-   
-    #conjugate gradient opt
-    norm_grad=norm(Q_G)
-    norm_grad0=norm(Q_G_old)
-    beta=(norm_grad^2)/(norm_grad0^2)
-    if op_step==1
-        direction=Q_G;
-    else
-        direction=Q_G+beta*direction_old;
-    end
-    for ls_step=0:ls_max-1  
-        alpha=alpha0*(ls_ratio^ls_step);
-        W_new=exp(-alpha*direction)*W;
-        E=cost_f(W_new);
-        
-        println("   Conjugate gradient opt, LS="*string(ls_step+1)*", "*"E="*string(E));flush(stdout);
-        improvement=E-E0;
-        if E<E0
-            improved=true
-            W=W_new;
-            break
+    M=rand(size(W,1),size(W,1))+im*rand(size(W,1),size(W,1));
+    W=exp(im*noise*(M+M'))*W;
+
+
+
+    E0=0;
+    Q_G_old=zeros(size(W));
+    direction_old=zeros(size(W,1),size(W,1));
+    for op_step=1:1000
+        E0=cost_f(W);
+        println("Optimization "*string(op_step)*", E0="*string(E0));flush(stdout);
+        grad = gradient(Flux.params(W)) do
+            cost_f(W)
         end
-    end
-    direction0=direction;
+        g=grad[W];
+        Q_G=g*W'-W*g';
+        improved=false;
+        improvement=0;
     
-
-    if improved
-    else
-        #gradient opt
-        for ls_step=0:ls_max-1    
+        #conjugate gradient opt
+        norm_grad=norm(Q_G)
+        norm_grad0=norm(Q_G_old)
+        beta=(norm_grad^2)/(norm_grad0^2)
+        if op_step==1
+            direction=Q_G;
+        else
+            direction=Q_G+beta*direction_old;
+        end
+        for ls_step=0:ls_max-1  
             alpha=alpha0*(ls_ratio^ls_step);
-            W_new=exp(-alpha*Q_G)*W;
+            W_new=exp(-alpha*direction)*W;
             E=cost_f(W_new);
             
-            println("   Gradient opt, LS="*string(ls_step+1)*", "*"E="*string(E));flush(stdout);
+            println("   Conjugate gradient opt, LS="*string(ls_step+1)*", "*"E="*string(E));flush(stdout);
             improvement=E-E0;
             if E<E0
                 improved=true
@@ -115,11 +107,74 @@ for op_step=1:1000
                 break
             end
         end
+        direction_old=direction;
+        Q_G_old=Q_G;
+
+        if improved
+        else
+            #gradient opt
+            for ls_step=0:ls_max-1    
+                alpha=alpha0*(ls_ratio^ls_step);
+                W_new=exp(-alpha*Q_G)*W;
+                E=cost_f(W_new);
+                
+                println("   Gradient opt, LS="*string(ls_step+1)*", "*"E="*string(E));flush(stdout);
+                improvement=E-E0;
+                if E<E0
+                    improved=true
+                    W=W_new;
+                    break
+                end
+            end
+        end
+        if -improvement<1e-7
+            break;
+        end
+
     end
-    if -improvement<1e-7
-        break;
+    return W,E0
+end
+
+
+noise=0;
+W,E0=line_search(W,noise);
+
+for cn=1:noise_ite
+    noise=0.1;
+    W_updated,E0_updated=line_search(W,noise);
+    if E0_updated<E0
+        E0=E0_updated;
+        W=W_updated;
+    end
+
+    noise=0.05;
+    W_updated,E0_updated=line_search(W,noise);
+    if E0_updated<E0
+        E0=E0_updated;
+        W=W_updated;
+    end
+
+    noise=0.01;
+    W_updated,E0_updated=line_search(W,noise);
+    if E0_updated<E0
+        E0=E0_updated;
+        W=W_updated;
+    end
+
+    noise=0.005;
+    W_updated,E0_updated=line_search(W,noise);
+    if E0_updated<E0
+        E0=E0_updated;
+        W=W_updated;
+    end
+
+    noise=0.001;
+    W_updated,E0_updated=line_search(W,noise);
+    if E0_updated<E0
+        E0=E0_updated;
+        W=W_updated;
     end
 
 end
 
-
+println(E0)
