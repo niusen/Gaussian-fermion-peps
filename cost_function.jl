3c9a55e7384ba581d51d5f9857c159f1d5aa6bc5
niusen <1501110187@pku.edu.cn>
2024-05-26 20:55:26 -0700

    Update cost_function.jl

--- cost_function.jl
+++ cost_function.jl
@@ -1,3 +1,45 @@
+function Hofstadter_triangle_2site(t,Lx,Ly,P,M,kxs,kys,W)
+    N=Lx*Ly;
+
+    C_T=2*W*W'-Matrix(I, P+4*M, P+4*M);
+    A=C_T[1:P,1:P];
+    B=C_T[1:P,P+1:P+4*M];
+    D=C_T[P+1:P+4*M,P+1:P+4*M];
+
+    phi=pi/2;
+    #define pauli matrix
+    sigmax=[0 1;1 0];
+    sigmay=[0 -im;im 0];
+    sigmaz=[1 0;0 -1];
+
+    E=0;
+    for ca=1:Lx
+        for cb=1:Ly
+            kx=kxs[ca];
+            ky=kys[cb];
+            C_in_1=[zeros(M,M) exp(im*kx)*Matrix(I, M, M); exp(-im*kx)*Matrix(I, M, M) zeros(M,M)];
+            C_in_2=[zeros(M,M) exp(im*ky)*Matrix(I, M, M); exp(-im*ky)*Matrix(I, M, M) zeros(M,M)];
+            C_in=[C_in_1 zeros(2*M,2*M); zeros(2*M,2*M) C_in_2];
+
+            C_out=A-B*pinv(D+C_in)*B';
+            rho_out=(C_out+Matrix(I, P, P))/2;
+
+            hAA=2*t*cos(ky);
+            hAB=t*(exp(im*phi)+exp(-im*phi)*exp(im*kx)-exp(im*ky)+exp(-im*ky+im*kx));
+            hk=[hAA hAB;hAB' -hAA];
+            
+
+            E=E+real(tr(hk*rho_out));
+            #println(eigvals(C_out))
+        end
+    end
+    E=E/N;
+
+    return E
+
+end
+
+
 function square_dirac(Lx,Ly,P,M,kxs,kys,W)
     N=Lx*Ly;
 
